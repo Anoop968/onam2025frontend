5df5845ac480992691d1c8f2b3acd36f359c3713
Anoop968 <an2177287@gmail.com>
2025-09-03 05:02:15 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 // Backend API URL
-const API_URL = 'http://127.0.0.1:5000/api';
+const API_URL = 'https://onam2025backend.onrender.com/api';
+
 
 // Hardcoded admin credentials
 const ADMIN_CREDENTIALS = {
@@ -157,3 +158,4 @@ function logout() {
 }
 
 window.onload = loadGames;
+
